bea32b0a114cf3152d62b7dcf03bbf2f28d17964
Cody Neiman <neiman1010222@gmail.com>
2019-03-30 16:40:14 -0400

    Generate guard tutorial slides

--- src/app/guards/tutorial.guard.ts
+++ src/app/guards/tutorial.guard.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TutorialGuard implements  {
-  
+export class TutorialGuard implements CanActivate {
+  constructor(private storage: Storage, private router: Router) {}
+
+  async canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    const isComplete = await this.storage.get('tutorialComplete');
+
+    if (!isComplete) {
+      this.router.navigateByUrl('/tutorial');
+    }
+
+    return isComplete;
+  }
 }
