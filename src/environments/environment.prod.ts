99177961f1f3df16e303a28921790a72f2a6fcc0
Cody Neiman <neiman1010222@gmail.com>
2019-03-30 11:21:42 -0400

    Add firebase webapp information

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA1y-orjl4TiDROoHTdPSvvJyVjf2Jjdws',
+    authDomain: 'ionic-demo-app-x.firebaseapp.com',
+    databaseURL: 'https://ionic-demo-app-x.firebaseio.com',
+    projectId: 'ionic-demo-app-x',
+    storageBucket: 'ionic-demo-app-x.appspot.com',
+    messagingSenderId: '847425801415'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA1y-orjl4TiDROoHTdPSvvJyVjf2Jjdws',
+    authDomain: 'ionic-demo-app-x.firebaseapp.com',
+    databaseURL: 'https://ionic-demo-app-x.firebaseio.com',
+    projectId: 'ionic-demo-app-x',
+    storageBucket: 'ionic-demo-app-x.appspot.com',
+    messagingSenderId: '847425801415'
+  }
 };
 
 /*
